76969c4aba8af1e5fab3e27c85fdb402d3f6ade5
Nathaniel Chang <namachan@ucsc.edu>
2020-12-23 15:40:24 -0800

    added project cards and modal for resume

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,23 @@
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml11 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+var textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml11 .line',
-    scaleY: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 700
-  })
-  .add({
-    targets: '.ml11 .line',
-    translateX: [1, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
-    easing: "easeOutExpo",
-    duration: 700,
-    delay: 800
-  }).add({
-    targets: '.ml11 .letter',
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
     opacity: [0,1],
     easing: "easeOutExpo",
-    duration: 1000,
-    offset: '-=775',
-    delay: (el, i) => 50 * (i+1)
+    duration: 1200,
+    delay: (el, i) => 700 + 30 * i
   }).add({
-    targets: '.ml11',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
+    targets: '.ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
   });
+
+  
